7d0d6765a04945fa487b8209781abab48e4677f5
Ubuntu <ubuntu@ip-172-26-6-16.us-east-2.compute.internal>
2018-01-07 04:53:14 +0000

    un-commented __tablename__ for User

--- Database.py
+++ Database.py
@@ -15,7 +15,7 @@ secret_key = ''.join(
 
 # create User table
 class User(db.Model):
-    # __tablename__ = 'user'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True)
     picture = db.Column(db.String())
